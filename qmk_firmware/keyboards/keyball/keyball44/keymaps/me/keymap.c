074de23d9c44db3ac3ee77c9764f2e21b28eed66
takahiromorooka <t.morooka32@gmail.com>
2025-03-26 00:16:43 +0900

    Update test_combo1 definition in keymap.c to use new key values and adjust associated combo action

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/me/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/me/keymap.c
@@ -70,9 +70,9 @@ void oledkit_render_info_user(void) {
 }
 #endif
 
-const uint16_t PROGMEM test_combo1[] = {LSFT_T(KC_A), LT(1, KC_B), COMBO_END};
+const uint16_t PROGMEM test_combo1[] = {KC_L, KC_SCLN , COMBO_END};
 const uint16_t PROGMEM test_combo2[] = {LSFT_T(KC_A), LT(1, KC_B), KC_C, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
-    COMBO(test_combo1, KC_ESC),
+    COMBO(test_combo1, S(KC_7) ),
     COMBO(test_combo2, KC_TAB),
 };
