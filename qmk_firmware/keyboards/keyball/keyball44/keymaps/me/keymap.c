00b1b682dd8e87ea695172e4c83befd90b8676b7
takahiromorooka <t.morooka32@gmail.com>
2025-03-25 23:30:10 +0900

    Add key combos for test functionality in keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/me/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/me/keymap.c
@@ -53,6 +53,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
+const uint16_t PROGMEM test_combo1[] = {LSFT_T(KC_A), LT(1, KC_B), COMBO_END};
+const uint16_t PROGMEM test_combo2[] = {LSFT_T(KC_A), LT(1, KC_B), KC_C, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(test_combo1, KC_ESC)
+    COMBO(test_combo2, KC_TAB)
+};
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
     keyball_set_scroll_mode(get_highest_layer(state) == 3);
